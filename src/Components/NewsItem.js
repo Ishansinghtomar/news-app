1a2a2215e2326ef43a3680f9fea5e81c302a4231
shivam-gupta-ttn <shivam.gupta@tothenew.com>
2021-12-22 21:58:37 +0530

    card CSS fix and responsive

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
    
         <div className="card-container" >
           <img src={image} className="card-image" alt="..." />
-          <div className="">
-            <h5 className="">{title}</h5>
-            <p className="">{description}</p>
-            <p className=""><small className="">By {author ? author : "uknown"} On {new Date(date).toGMTString()}</small></p>
-            <a rel="noreferrer" href={compurl} target="_blank" className="">Read Complete News</a>
+          <div className="card-info">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-desc">{description}</p>
+            <p className="card-auth"><small className="">By {author ? author : "uknown"} On {new Date(date).toGMTString()}</small></p>
+           <button className='fourth'> <a rel="noreferrer" href={compurl} target="_blank" className="card-link">Read Complete News</a></button>
           </div>
         </div>
       
